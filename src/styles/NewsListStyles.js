00d301f21400f76c4b2701e1da585a262a918d4c
Xelp <xelp@Xelps-iMac.local>
2019-05-24 13:04:19 +0530

    changes done

--- src/styles/NewsListStyles.js
+++ src/styles/NewsListStyles.js
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 const NewsListStyles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#d3d3d3"
+    backgroundColor: "#dcdcdc"
   },
   loaderContainer: {
     flex: 1,
@@ -11,6 +11,11 @@ const NewsListStyles = StyleSheet.create({
   },
   list: {
     padding: 5
+  },
+  loaderText: {
+    marginTop: 10,
+    fontSize: 14,
+    color: "black"
   }
 });
 export default NewsListStyles;
